7304b1be0c55928f40434b37c05a99db5a34ce10
raspberry-omer <kralabitayfa2@gmail.com>
2021-03-28 21:58:19 +0300

    commit message from python script

--- automated_push.py
+++ automated_push.py
@@ -1,7 +1,14 @@
 from shutil import copyfile
+from datetime import datetime
 import thingspeak  #pip3 install thingspeak
-import os, inspect
+import os, inspect, json, threading
 import git
+
+channel_id = "1341788" # Kendi kanal ID'nizi yazınız
+write_key  = "UGT4QQYNQMX13Q5V" # Kendi WRITE KEY değerinizi yazınız.
+read_key    = "GKPNYYQMPOSSHAGO" # Kendi YOUR API KEY değerinizi yazınız.
+channelRead = thingspeak.Channel(id=channel_id,api_key=read_key)
+channelWrite = thingspeak.Channel(id=channel_id,api_key=write_key)
 
 def getProjectDirectoryPath():
     # print (inspect.getfile(inspect.currentframe()))  # script filename (usually with path)
@@ -9,10 +16,10 @@ def getProjectDirectoryPath():
 
 os.chdir(getProjectDirectoryPath())
 
-# repository = Repo.init(os.getcwd())
-# print(repository)
-
 def git_push():
+    # repository = Repo.init(os.getcwd())
+    # print(repository)
+
     repo = git.Repo()
     # print(repo)
 
@@ -21,18 +28,48 @@ def git_push():
     
     # repo.git.push()
     os.system("git push -u origin main") 
+    print(datetime.now().strftime("%m_%d_%Y-%H_%M_%S"), "Push process is completed.")
 
 def copy_log_to_dir():
     log_name = "binance-trader.log"
     if os.path.isfile(log_name):
-        print ("File exist")
+        print(datetime.now().strftime("%m_%d_%Y-%H_%M_%S"), "File exist.")
         os.remove(log_name)
-        print ("File is removed")
+        print(datetime.now().strftime("%m_%d_%Y-%H_%M_%S"), "File is removed.")
     else:
-        print ("File not exist")
+        print(datetime.now().strftime("%m_%d_%Y-%H_%M_%S"), "File not exist.")
 
     copyfile("../../binance-trader/" + log_name  ,os.getcwd() + "/" + log_name)
-    print ("File is copied")
+    print(datetime.now().strftime("%m_%d_%Y-%H_%M_%S"), "File is copied.")
 
-copy_log_to_dir()
-git_push()
+def writeZero():
+    try:
+        data = {
+            "channel_id": channel_id,
+            "field1": "0"
+        }
+        channelWrite.update(data)
+    except:
+        pass
+
+def shouldPush():
+    try:
+        readFromTS = channelRead.get_field_last(field='field1')
+        fieldDict = json.loads(readFromTS)
+        fieldLast = int(fieldDict["field1"])
+
+        if fieldLast == 1:
+            print(datetime.now().strftime("%m_%d_%Y-%H_%M_%S"), "Request arrived.")
+            copy_log_to_dir()
+            git_push()
+            writeZero()
+        else:
+            print(datetime.now().strftime("%m_%d_%Y-%H_%M_%S"), "Waiting for request.")
+    except:
+        pass
+
+def repeatShouldPush():
+    shouldPush()
+    threading.Timer(5 * 60, repeatShouldPush).start()
+
+repeatShouldPush()
