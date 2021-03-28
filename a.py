7790f4b204e666ca01c15f641cc58211f47d94f6
raspberry-omer <kralabitayfa2@gmail.com>
2021-03-28 20:46:36 +0300

    commit message from python script

---
+++ a.py
@@ -0,0 +1,19 @@
+import os, inspect
+import git
+
+def getProjectDirectoryPath():
+    # print (inspect.getfile(inspect.currentframe()))  # script filename (usually with path)
+    return os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))   # script directory
+
+os.chdir(getProjectDirectoryPath())
+
+# repository = Repo.init(os.getcwd())
+# print(repository)
+
+repo = git.Repo()
+# print(repo)
+
+repo.git.add('--all')
+repo.git.commit('-m', 'commit message from python script', author='raspberry-omer <kralabitayfa2@gmail.com>')
+
+# repo.git.push("origin", "HEAD:refs/for/master")
